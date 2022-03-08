c06570256bb2566efb47154203187df67f2b7acc
MK <46935118+TheDeadGamer101@users.noreply.github.com>
2022-03-08 21:59:17 +0200

    Prototyped info page

--- public/main.js
+++ public/main.js
@@ -1,3 +1,17 @@
 function pressed() {
     window.location.href = "https://khkinfokiri.sendsmaily.net/template/preview/id/48?liitu";
 }
+
+function toTp() {
+    window.location.href = "/tp"
+}
+
+function toSisse(to) {
+    if (to == "ita") {
+        window.location.href = "https://voco.ee/eriala/it-akadeemia-tarkvaraarendaja-3/";
+
+    } else {
+        window.location.href = "https://voco.ee/eriala/it-susteemide-spetsialist-projekt/";
+    }
+
+}
