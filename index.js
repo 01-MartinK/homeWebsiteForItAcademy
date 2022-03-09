69b63857dc599204c381509d7bcb706228e5a514
frastsytt <frastsytt@gmail.com>
2022-03-09 06:04:43 +0200

    timetable integration

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const router = express.Router();
+const timetable = require('./public/timetable/timetable.js');
+const fetch = require('node-fetch');
 
 // bodyParser Use
 const bodyParser = require('body-parser');
@@ -22,7 +25,15 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.get('/timetable', async (req, res) => {
+    res.render('timetable', {
+        require: require,
+        fetch: fetch
+    })
 
+
+
+});
 
 const server = app.listen(3010, () => {
     console.log(`Express running -> PORT ${server.address().port}`)
