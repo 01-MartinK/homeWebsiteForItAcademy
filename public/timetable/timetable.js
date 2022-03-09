d02120f3a5cd2e29561a4c83f368342e8fca09fd
frastsytt <frastsytt@gmail.com>
2022-03-09 13:58:20 +0200

    tra hea uni oli

--- public/timetable/timetable.js
+++ public/timetable/timetable.js
@@ -3,12 +3,18 @@ var XMLHttpRequest = require('xhr2');
 
 
 async function clickme() {
+    var selectorWeek = document.getElementById('week').value;
+    console.log(parseInt(selectorWeek.slice(-2)))
+    console.log(parseInt(selectorWeek.substring(-4)))
+    var pickerDate = getDateOfWeek(parseInt(selectorWeek.slice(-2)) + 1, parseInt(selectorWeek.substring(-4)))
+    console.log(new Date(pickerDate).toISOString())
     var element = document.getElementById('peepo').value;
-    var today = getFirstDayOfWeek(new Date());
-    var dd = String(today.getDate()).padStart(2, '0');
-        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-        var yyyy = today.getFullYear();
-    today = yyyy + '-' + mm + '-' + dd;
+
+    var dd = String(pickerDate.getDate()).padStart(2, '0');
+        var mm = String(pickerDate.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = pickerDate.getFullYear();
+    pickerDate = yyyy + '-' + mm + '-' + dd;
+    console.log(pickerDate)
 
     const dict = {
         'ITA19': 1433,
@@ -18,11 +24,10 @@ async function clickme() {
         'ITS21': 1582
     }
 
-    queryurl = `https://siseveeb.voco.ee/veebilehe_andmed/tunniplaan?grupp=${dict[element]}&nadal=${today}`;
+    queryurl = `https://siseveeb.voco.ee/veebilehe_andmed/tunniplaan?grupp=${dict[element]}&nadal=${pickerDate}`;
     console.log(queryurl)
 
     console.log(dict[element])
-    console.log(today)
     let body
     var oReq = new XMLHttpRequest();
     oReq.open("GET", queryurl);
@@ -45,7 +50,12 @@ async function clickme() {
                     console.log(cmp2)
                     Object.keys(this.response['tunnid'][paev]).forEach(paevaTund => {
                         compare = parseInt(this.response['tunnid'][paev][paevaTund]['tund'])
-                        table[0].rows[compare].cells[cmp2].innerText = this.response['tunnid'][paev][paevaTund]['aine'];
+                        var instring = ''
+                        instring += this.response['tunnid'][paev][paevaTund]['aine']
+                        //instring += `\n\n${this.response['tunnid'][paev][paevaTund]['opetaja']}`
+                        //instring += `\n\n${this.response['tunnid'][paev][paevaTund]['ruum']}`
+
+                        table[0].rows[compare].cells[cmp2].innerText = instring;
                         })
                     })
                 }
@@ -66,4 +76,10 @@ function getFirstDayOfWeek(d) {
     const diff = date.getDate() - day + (day === 0 ? -6 : 1);
 
     return new Date(date.setDate(diff));
+}
+
+function getDateOfWeek(w, y) {
+    let date = new Date(y, 0, (1 + (w - 1) * 7)); // Elle's method
+    date.setDate(date.getDate() + (1 - date.getDay())); // 0 - Sunday, 1 - Monday etc
+    return date
 }
